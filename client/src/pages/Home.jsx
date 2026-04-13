c7d3b4f2baafe457f407a6c3fc8f57e205927349
adarshthakur9240 <singhadadarsh9240@gmail.com>
2026-04-14 03:18:04 +0530

    Build Fix: Fixed case-sensitivity for Home.jsx and cleaned imports

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
 import AboutSection from "../components/AboutSection";
-import Features from "../components/Features";
 
 const Home = () => {
   return (
-    <div className="bg-[#050505] min-h-screen overflow-x-hidden font-['Inter']">
+    <div className="bg-[#050505] min-h-screen overflow-x-hidden font-sans">
       <Hero />
       <AboutSection />
-      <Features />
+      {/* Agar Features.jsx missing hai toh ye line error degi, isliye abhi hata di hai */}
     </div>
   );
 };
