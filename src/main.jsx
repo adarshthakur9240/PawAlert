6e06dcc95c5b92a95f59d5e6f119a21667bf5bce
adarshthakur9240 <singhadadarsh9240@gmail.com>
2026-04-15 20:52:12 +0530

    Fix: Wrapped App in BrowserRouter in main.jsx and resolved useLocation error

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // ✅ Added this
 import App from './App.jsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { Provider } from 'react-redux'
-import store from './store/store' // ✅ Fixed: Removed curly braces
+import store from './store/store'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -16,7 +17,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-        <App />
+        <BrowserRouter> 
+          <App />
+        </BrowserRouter>
       </ClerkProvider>
     </Provider>
   </React.StrictMode>,
