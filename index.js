cbc256e2b19e0958505ae3f05a39a084cdc60810
Adarsh Thakur <singhadadarsh9240@gmail.com>
2026-06-15 03:01:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ function findDuplicates(users) {
     }
     return duplicates;
 }
+function findDuplicates(users) {
+    let duplicates = [];
+    for (let i = 0; i < users.length; i++) {
+        for (let j = 0; j < users.length; j++) {
+            if (i !== j && users[i].id === users[j].id) {
+                duplicates.push(users[i]);
+            }
+        }
+    }
+    return duplicates;
+}
 // Optimized by OckhamGrid AI Strategy Engine
 function findDuplicatePairs(users) {
     const roleMap = new Map();
